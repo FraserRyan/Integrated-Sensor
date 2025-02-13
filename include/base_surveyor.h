42480166bd44d7cc627354c634a5c1945bdfbc95
joshthaw <78183428+joshthaw@users.noreply.github.com>
2025-02-12 19:57:47 -0500

    formatting

--- include/base_surveyor.h
+++ include/base_surveyor.h
@@ -31,27 +31,28 @@ SOFTWARE
 #include "WProgram.h"
 #endif
 
-class Surveyor_Base{
-	public:
-        enum Surveyor_type{
-            SURVEYOR_PH = 1,
-            SURVEYOR_DO,
-            SURVEYOR_ORP,
-            SURVEYOR_RTD
-        };
-        
-        virtual bool begin();
-	
-		virtual float read_voltage();
+class Surveyor_Base
+{
+public:
+    enum Surveyor_type
+    {
+        SURVEYOR_PH = 1,
+        SURVEYOR_DO,
+        SURVEYOR_ORP,
+        SURVEYOR_RTD
+    };
 
-    protected:
-	
-		uint8_t pin = A0;
-        
-		static const int volt_avg_len = 1000;
-        static const uint8_t EEPROM_SIZE_CONST = 16;
-		static const uint8_t magic_char = 0xAA;
-        
-        int16_t EEPROM_offset = 0;
+    virtual bool begin();
+
+    virtual float read_voltage();
+
+protected:
+    uint8_t pin = A0;
+
+    static const int volt_avg_len = 1000;
+    static const uint8_t EEPROM_SIZE_CONST = 16;
+    static const uint8_t magic_char = 0xAA;
+
+    int16_t EEPROM_offset = 0;
 };
 #endif
